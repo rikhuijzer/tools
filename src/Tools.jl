25b46db8066f49ee8d01a95311f30aaed1d1d702
Rik Huijzer <rikhuijzer@pm.me>
2021-03-13 17:50:01 +0100

    Fix CI?

--- src/Tools.jl
+++ src/Tools.jl
@@ -12,13 +12,10 @@ include("images.jl")
 # Convenient for calling `generate_content(; M)`.
 export M
 
-M = Tools
+const M = Tools
 
 function build()
     println("Building tools")
-    rm(build_dir; force=true, recursive=true)
-    mkpath(build_dir)
-
     Books.generate_content(; M=Tools, fail_on_error=true)
     Books.build_all()
 end
