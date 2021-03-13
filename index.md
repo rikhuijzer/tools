978d2e1c26bb407f8791ab6aa0c52401e5ae528a
Rik Huijzer <rikhuijzer@pm.me>
2021-03-13 17:55:04 +0100

    Define projects.default (#20)

--- index.md
+++ index.md
@@ -2,8 +2,8 @@
 
 [//]: # (This file is only included on the website.)
 
-This website shows the book Tools for Science and is also available as [PDF](/tools.pdf){target="_blank"}.
-It's still a work in progress, but I'm quite sure that it will be finished. 
+This website shows the book Tools for Science and is also available as [**PDF**](/tools.pdf){target="_blank"}.
+It's still a work in progress, but I'm quite sure that it will be finished.
 Feel free to browse around a bit and let me know if you spot any mistakes or have ideas on how to improve the book.
 
 The source code is available at [GitHub](https://github.com/rikhuijzer/tools){target="_blank"}.
--- src/images.jl
+++ src/images.jl
@@ -1,24 +1,3 @@
-using Colors
-using Compose
-
-function write_svg(file, p; w=6inch, h=4inch) 
-    dir = joinpath(build_dir, "im")
-    mkpath(dir)
-    filename = "$file.svg"
-    path = joinpath(dir, filename)
-    draw(SVG(path, w, h), p)
-    joinpath("im", filename)
-end
-
-"""
-    svg2png(svg_path, png_path)
-
-Unlike ImageMagick, this program gets the text spacing right.
-"""
-function svg2png(svg_path, png_path)
-    run(`cairosvg $svg_path -o $png_path`)
-end
-
 function image_and_caption(file, p; caption=file, w=6inch, h=4inch)
     image_path = write_svg(file, p; w, h)
     text = """
@@ -30,13 +9,13 @@ line(x0, y0, x1, y1) = Compose.line([[(x0, y0), (x1, y1)]])
 
 function green_ci()
     c = context()
-    composition = compose(c, 
+    composition = compose(c,
         (c, text(0.07, 0.18, "John Doe"), fontsize(16px)),
         (c, rectangle(0.05, 0.05, 0.9, 0.2), stroke("black"), fill(colorant"#f1f8ff")),
         (c, text(0.07, 0.38, "I've added a test."), fontsize(16px)),
         (c, rectangle(0.05, 0.25, 0.9, 0.3), stroke("black"), fill("transparent")),
-        (c, circle(0.1, 0.65, 0.02), stroke("gray"), linewidth(1.5px), fill("white")),
-        (c, circle(0.1, 0.8, 0.02), stroke("gray"), linewidth(1.5px), fill("white")),
+        (c, Compose.circle(0.1, 0.65, 0.02), stroke("gray"), linewidth(1.5px), fill("white")),
+        (c, Compose.circle(0.1, 0.8, 0.02), stroke("gray"), linewidth(1.5px), fill("white")),
         (c, line(0.1, 0.55, 0.10, 0.9), stroke("gray"), linewidth(2px)),
         (c, line(0.07, 0.65, 0.13, 0.65), stroke("gray"), linewidth(2px)),
         (c, line(0.07, 0.8, 0.13, 0.8), stroke("gray"), linewidth(2px)),
@@ -49,6 +28,7 @@ function green_ci()
     Simplified view of the GitHub interface for pull-requests.
     It shows that John Doe did a failing commit and fixed it afterwards.
     """
-    file = "green_ci"
-    image_and_caption(file, composition; caption, w=4inch, h=2inch)
+
+    # Needs an update of Books.jl to pass caption.
+    return composition
 end
